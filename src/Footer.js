9a045773b90f48a562a704b65ea5f8e0f6e60734
MariaLizMatthaeus <94976037+MariaLizMatthaeus@users.noreply.github.com>
2022-05-22 10:07:05 +0200

    added final styling touches to bootstrap and fontawesome icons + element

--- src/Footer.js
+++ src/Footer.js
@@ -3,19 +3,19 @@ import React from "react";
 export default function Footer() {
   return (
     <small className="credits">
-      This project is
+      This project is{" "}
       <a
         href="https://github.com/MariaLizMatthaeus/react-weather-app"
         target="_blank"
         rel="noreferrer"
       >
         open-source code
-      </a>
+      </a>{" "}
       hosted on <a href="https://www.netlify.com/">Netlify</a> coded by Maria
-      Matthäus with 🖤 and caring support by
-      <a href="https://www.shecodes.io/"> SheCodes</a>
+      Matthäus with 🖤 and caring support by{" "}
+      <a href="https://www.shecodes.io/">SheCodes</a>
       <div>
-        image credits:
+        image credits:{" "}
         <a href="https://pixabay.com/illustrations/abstract-dust-background-wallpaper-5719221/">
           Tomislav Jakupec
         </a>
--- src/SearchEngine.js
+++ src/SearchEngine.js
@@ -32,25 +32,32 @@ export default function SearchEngine() {
   }
 
   let form = (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="search"
-        placeholder="Type a city name ..."
-        className="form-control border-end-0 border"
-        autoComplete="off"
-        onChange={updateCity}
-      />
-
-      <button className="btn btn-large" type="submit">
-        <FontAwesomeIcon icon={solid("search")} />
-      </button>
-      <button
-        type="button"
-        className="btn current-location"
-        id="current-button"
-      >
-        <FontAwesomeIcon icon={solid("map-marked")} />
-      </button>
+    <form className="mb-3" onSubmit={handleSubmit}>
+      <div className="row">
+        <div className="input-group col-3">
+          <input
+            type="search"
+            placeholder="Type a city name ..."
+            className="form-control border-end-0 border"
+            autoComplete="off"
+            onChange={updateCity}
+          />
+          <span className="input-group-append">
+            <button className="btn btn-large" type="submit">
+              <FontAwesomeIcon icon={solid("search")} inverse size="2x" />
+            </button>
+          </span>
+          <div className="col-2">
+            <button
+              type="button"
+              className="btn current-location"
+              id="current-button"
+            >
+              <FontAwesomeIcon icon={solid("map-marked")} inverse size="2x" />
+            </button>
+          </div>
+        </div>
+      </div>
     </form>
   );
 
